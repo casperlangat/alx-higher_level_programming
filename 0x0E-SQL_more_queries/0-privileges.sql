84f39ff87ea871d6e14a902148f70678c0c0a73b
casperlangat <langatcasper@gmail.com>
2023-03-15 13:57:15 -0700

    My privileges!

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,1 +1,2 @@
-0-privileges.sql: MySQL script that lists all privileges users user_0d_1 and user_0d_2
+0-privileges.sql: MySQL script that lists all privileges of the users
+user_0d_1 and user_0d_2
