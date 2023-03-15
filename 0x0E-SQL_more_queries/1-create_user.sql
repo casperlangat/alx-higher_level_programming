d37d723e72e4add46259c95e533eb5cc7a4792be
casperlangat <langatcasper@gmail.com>
2023-03-15 14:24:19 -0700

    Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- Creates the user user_0d_1 with all privileges
 CREATE USER
-	IF NOT EXIST 'user_0d_1'@'localhost'
+	IF NOT EXISTS 'user_0d_1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
 	0N *.*
