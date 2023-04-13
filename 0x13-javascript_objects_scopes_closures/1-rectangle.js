cf1de20e5b4ad5861a3ca97e797f654ff4144175
casperlangat <langatcasper@gmail.com>
2023-04-13 08:33:29 -0700

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 //Js script to define a class and initialize them
-module.exports = class Rectangle{
+module.exports = class Rectangle {
 	constructor (w, h) {
 	this.width = w;
 	this.height = h;
-}
+	}
 };
